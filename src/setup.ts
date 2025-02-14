1f9a6656ce8f47e23e862c7e0bcb2f6c824fabb4
Charron Théo <2t0h0e2o@gmail.com>
2025-02-14 15:44:54 +0100

    Parametized migrations (#1)
    * Parametized migrations
    
    * 1.1.0

--- src/setup.ts
+++ src/setup.ts
@@ -1,39 +1,65 @@
-import type { PoolClient } from "pg";
+import type { PoolClient } from "pg"
 
-import { dirname, join } from "node:path";
+import { dirname, join } from "node:path"
 
-import Postgrator from "postgrator";
+import Postgrator from "postgrator"
 
-const MIGRATIONS_FOLDER = join(dirname(__dirname), "migrations");
+const MIGRATIONS_FOLDER = join(dirname(__dirname), "migrations")
+const DEFAULT_SCHEMA = "graphile_worker_helpers"
+const DEFAULT_GRAPHILE_SCHEMA = "graphile_worker"
 
-const getPostgrator = async (pgClient: PoolClient) => {
-  await pgClient.query(
-    /* sql */ `CREATE SCHEMA IF NOT EXISTS graphile_worker_helpers`
-  );
+interface SchemaOptions {
+  helpersSchema?: string
+  graphileSchema?: string
+}
+
+const getPostgrator = async (pgClient: PoolClient, schemas?: SchemaOptions) => {
+  const helpersSchema = schemas?.helpersSchema ?? DEFAULT_SCHEMA
+  const escapedSchema = pgClient.escapeIdentifier(helpersSchema)
+
+  await pgClient.query(/* sql */ `CREATE SCHEMA IF NOT EXISTS ${escapedSchema}`)
 
   const {
     rows: [{ currentDatabase }],
   } = await pgClient.query<{ currentDatabase: string }>(
     /* sql */ `SELECT current_database() AS "currentDatabase"`
-  );
+  )
 
   return new Postgrator({
     driver: "pg",
     database: currentDatabase,
     migrationPattern: join(MIGRATIONS_FOLDER, "*"),
-    schemaTable: "graphile_worker_helpers.migrations",
-    execQuery: (sql) => pgClient.query(sql),
-  });
-};
+    schemaTable: `${helpersSchema}.migrations`,
+    execQuery: (sql) =>
+      pgClient.query(
+        sql
+          .replaceAll(
+            ":GRAPHILE_SCHEMA",
+            pgClient.escapeIdentifier(
+              schemas?.graphileSchema ?? DEFAULT_GRAPHILE_SCHEMA
+            )
+          )
+          .replaceAll(":HELPERS_SCHEMA", escapedSchema)
+      ),
+  })
+}
 
-export const setup = async (pgClient: PoolClient) => {
-  const postgrator = await getPostgrator(pgClient);
+export const setup =
+  (schemas?: SchemaOptions) => async (pgClient: PoolClient) => {
+    const postgrator = await getPostgrator(pgClient, schemas)
 
-  await postgrator.migrate();
-};
+    await postgrator.migrate()
+  }
 
-export const remove = async (pgClient: PoolClient) => {
-  const postgrator = await getPostgrator(pgClient);
+export const remove =
+  (schemas?: SchemaOptions) => async (pgClient: PoolClient) => {
+    const postgrator = await getPostgrator(pgClient, schemas)
 
-  await postgrator.migrate("0000");
-};
+    await postgrator.migrate("0000")
+
+    await pgClient.query(
+      /* sql */ `DROP SCHEMA ${pgClient.escapeIdentifier(
+        schemas?.helpersSchema ?? DEFAULT_SCHEMA
+      )} CASCADE`
+    )
+  }
